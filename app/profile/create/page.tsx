865d4f7b74e63631de8fe29936266634ced0b220
Dano <danocolombo@gmail.com>
2024-12-14 20:01:26 -0500

    createProfile w/components

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,28 +1,40 @@
 import React from 'react';
-import { Label } from '@/components/ui/label';
-import { Input } from '@/components/ui/input';
+import FormContainer from '@/components/form/FormContainer';
+import FormInput from '@/components/form/FormInput';
 import { Button } from '@/components/ui/button';
-
-const createProfileAction = async (formData: FormData) => {
+import { SubmitButton } from '@/components/form/Buttons';
+const createProfileAction = async (prevState: any, formData: FormData) => {
     'use server';
     const firstName = formData.get('firstName') as string;
-    console.log(firstName);
+    console.log('FIRST NAME:', firstName);
+    return { message: 'Profile created' };
 };
 
 function CreateProfilePage() {
     return (
         <section>
             <h1 className='text-2xl font-semibold mb-8 capitalize'>new user</h1>
-            <div className='border p-8 rounded-md max-w-lg'>
-                <form action={createProfileAction}>
-                    <div className='mb-2'>
-                        <Label htmlFor='firstName'>First Name</Label>
-                        <Input id='firstName' name='firstName' type='text' />
+            <div className='border p-8 rounded-md'>
+                <FormContainer action={createProfileAction}>
+                    <div className='grid md:grid-cols-2 gap-4'>
+                        <FormInput
+                            label='First Name'
+                            name='firstName'
+                            type='text'
+                        />
+                        <FormInput
+                            label='Last Name'
+                            name='lastName'
+                            type='text'
+                        />
+                        <FormInput
+                            label='Username'
+                            name='username'
+                            type='text'
+                        />
                     </div>
-                    <Button type='submit' size='lg'>
-                        Create Profile
-                    </Button>
-                </form>
+                    <SubmitButton text='Create Profile' className='mt-8' />
+                </FormContainer>
             </div>
         </section>
     );
