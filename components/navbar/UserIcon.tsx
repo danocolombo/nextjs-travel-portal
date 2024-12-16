475acdd09c3df42d58d6884af2320d642e9e61b9
Dano <danocolombo@gmail.com>
2024-12-16 09:15:13 -0500

    getting user image from subabase/saved from clerk

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import { fetchProfileImage } from '@/utils/actions';
 import { LuUser } from 'react-icons/lu';
-function UserIcon() {
+async function UserIcon() {
+    const profileImage = await fetchProfileImage();
+    if (profileImage) {
+        return (
+            <img
+                src={profileImage}
+                alt='Profile Image'
+                className='h-6 w-6 rounded-full object-cover'
+            />
+        );
+    }
     return <LuUser className='h=6 w-6 bg-primary rounded-full text-white' />;
 }
 
