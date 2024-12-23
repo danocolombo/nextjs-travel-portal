e5fa40df920ceaed52860249355bba91bed643f0
Dano <danocolombo@gmail.com>
2024-12-23 14:54:36 -0500

    home displaying properties

--- components/card/PropertyRating.tsx
+++ components/card/PropertyRating.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import { fetchPropertyRating } from '@/utils/actions';
+import { FaStar } from 'react-icons/fa';
 
-function PropertyRating() {
-    return <div>PropertyRating</div>;
+async function PropertyRating({
+    propertyId,
+    inPage,
+}: {
+    propertyId: string;
+    inPage: boolean;
+}) {
+    // const { rating, count } = await fetchPropertyRating(propertyId);
+    const rating = 4.8;
+    const count = 88;
+
+    // if (count === 0) return null;
+    const className = `flex gap-1 items-center ${
+        inPage ? 'text-md' : 'text-xs'
+    }`;
+    const countText = count === 1 ? 'review' : 'reviews';
+    const countValue = `(${count}) ${inPage ? countText : ''}`;
+    return (
+        <span className={className}>
+            <FaStar className='w-3 h-3' />
+            {rating} {countValue}
+        </span>
+    );
 }
 
 export default PropertyRating;
