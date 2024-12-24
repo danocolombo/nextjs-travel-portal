46182d9c5018e4e3dfee79ce397e7aaa67c15c1c
Dano <danocolombo@gmail.com>
2024-12-24 13:07:53 -0500

    favorites button working

--- utils/actions.ts
+++ utils/actions.ts
@@ -197,3 +197,48 @@ export const fetchProperties = async ({
     });
     return properties;
 };
+export const fetchFavoriteId = async ({
+    propertyId,
+}: {
+    propertyId: string;
+}) => {
+    const user = await getAuthUser();
+    const favorite = await db.favorite.findFirst({
+        where: {
+            propertyId,
+            profileId: user.id,
+        },
+        select: {
+            id: true,
+        },
+    });
+    return favorite?.id || null;
+};
+export const toggleFavoriteAction = async (prevState: {
+    propertyId: string;
+    favoriteId: string | null;
+    pathname: string;
+}) => {
+    const { propertyId, favoriteId, pathname } = prevState;
+    const user = await getAuthUser();
+    try {
+        if (favoriteId) {
+            await db.favorite.delete({
+                where: {
+                    id: favoriteId,
+                },
+            });
+        } else {
+            await db.favorite.create({
+                data: {
+                    propertyId,
+                    profileId: user.id,
+                },
+            });
+        }
+        revalidatePath(pathname);
+        return { message: favoriteId ? 'Favorite removed' : 'Favorite set' };
+    } catch (error) {
+        return renderError(error);
+    }
+};
