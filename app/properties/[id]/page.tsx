d2ea05a16c595f0943b9ec06c8144ff15a78b429
Dano <danocolombo@gmail.com>
2024-12-26 07:55:11 -0500

    host and description added to property details page

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,8 +1,13 @@
 import FavoriteToggleButton from '@/components/card/FavoriteToggleButton';
 import PropertyRating from '@/components/card/PropertyRating';
+import BookingCalendar from '@/components/properties/BookingCalendar';
 import BreadCrumbs from '@/components/properties/BreadCrumbs';
+import Description from '@/components/properties/Description';
 import ImageContainer from '@/components/properties/ImageContainer';
+import PropertyDetails from '@/components/properties/PropertyDetails';
 import ShareButton from '@/components/properties/ShareButton';
+import UserInfo from '@/components/properties/UserInfo';
+import { Separator } from '@/components/ui/separator';
 import { fetchPropertyDetails } from '@/utils/actions';
 import { redirect } from 'next/navigation';
 import React from 'react';
@@ -14,6 +19,8 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
     }
     const { baths, bedrooms, beds, guests } = property;
     const details = { baths, bedrooms, beds, guests };
+    const firstName = property.profile.firstName;
+    const profileImage = property.profile.profileImage;
     return (
         <section>
             <h2>PropertyDetailsPage</h2>
@@ -38,9 +45,14 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
                         <h1 className='text-xl font-bold'>{property.name}</h1>
                         <PropertyRating inPage propertyId={property.id} />
                     </div>
+                    <PropertyDetails details={details} />
+                    <UserInfo profile={{ firstName, profileImage }} />
+                    <Separator className='mt-4' />
+                    <Description description={property.description} />
                 </div>
                 <div className='lg:col-span-4 flex flex-col items-center'>
                     {/* calendar */}
+                    <BookingCalendar />
                 </div>
             </section>
         </section>
